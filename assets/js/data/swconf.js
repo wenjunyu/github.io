42a4e3adad62be7aa6a1605e45a59abc84f68b46
jun-yu <yujun.zl@163.com>
2024-07-16 21:36:05 +0800

    update self info

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1721052301',resources: [
+    cacheName: 'chirpy-1721136861',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
